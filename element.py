3768e3baebceb8ebd45f529b5d0b26955388e267
faymaz <fethi.aymaz@gmail.com>
2023-07-30 17:05:49 +0200

    first commit

---
+++ element.py
@@ -0,0 +1,14 @@
+#
+#   PyThon Lerning  @Faymaz
+#
+#
+import periodictable
+
+# Get details of an element by atomic number
+Atomic_No = int(input("Enter Element Atomic No :"))
+element = periodictable.elements[Atomic_No]
+print('Atomic number:', element.number)
+print('Symbol:', element.number)
+print('Name:', element.name)
+print('Atomic mass:', element.mass)
+print('Density:', element.density)
---
+++ picturebackground.py
@@ -0,0 +1,10 @@
+#
+#   PyThon Lerning  @Faymaz
+#
+#
+from PIL import Image
+input_path = 'resim.jpg'
+output_path = 'resim.png'
+inp = Image.open(input_path)
+output = remve(inp)
+output.save(output_path)
